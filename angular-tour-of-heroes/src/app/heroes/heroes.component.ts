97fbc46f3d7e8dbf0554ace9ef1660ae9cc67a78
Jian He <He.Jian@ufh.com.cn>
2020-01-17 17:33:00 +0800

    显示英雄列表（做了一半）

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core"; // 从 Angular 核心库中导入 Component 符号，并为组件类加上 @Component 装饰器
 import { Hero } from "../hero"; // 导入 Hero 类
+import { HEROES } from "../mock-heroes";
 
-@Component({ // 装饰器函数，用于为该组件指定 Angular 所需的元数据
+@Component({
+  // 装饰器函数，用于为该组件指定 Angular 所需的元数据
   selector: "app-heroes", // 组件的选择器（CSS 元素选择器）
   templateUrl: "./heroes.component.html", // 组件模板文件的位置
   styleUrls: ["./heroes.component.css"] // 组件私有 CSS 样式表文件的位置
@@ -11,6 +13,8 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: "Windstrom"
   };
+
+  heroes = HEROES;
 
   constructor() {}
 
